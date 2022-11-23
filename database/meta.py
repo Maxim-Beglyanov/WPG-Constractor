36487d787b0eb8e0e84dba43b57085498310f367
Maxim-Beglyanov <mbeglanov@gmail.com>
2022-11-24 00:26:22 +0700

    Перенес первую версию Table в meta.py. Добавил select в Table из table.py

--- database/meta.py
+++ database/meta.py
@@ -1,6 +1,6 @@
 import asyncio, re
 from enum import Enum
-from dataclasses import asdict, dataclass
+from dataclasses import asdict, dataclass, field
 from types import MappingProxyType
 from typing import Any, Callable, Coroutine, Iterable
 
@@ -15,20 +15,24 @@ class TablesAttitude(Enum):
     ManyToMany = 'list[Table] and list[Table]'
 TA = TablesAttitude
 
+Table = type
 @dataclass(frozen=True)
 class Field:
     sql_type: str = ...
-    type: 'Table' = ...
+    type: Table = ...
     default: Any = ...
     unique: bool = False
     not_null: bool = False
 
     attitude: TA = TA.Simple
-    init: Callable[[], Coroutine] = ...
-    insert: Callable[[], Coroutine] = ...
+    init: Callable[[], Coroutine] = field(default=..., repr=False)
+    insert: Callable[[], Coroutine] = field(default=..., repr=False)
 
 class MetaTable(type):
-    def __init__(cls, clsname, superclasses, attributedict):
+    def __init__(cls, clsname, superclasses, attributedict): 
+        if clsname == 'Table': return
+        
+        _switch_field_types(cls, clsname)
         cls.table = _clstable(clsname)
 
         fields = {}
@@ -38,6 +42,16 @@ class MetaTable(type):
             setattr(cls, key, field)
             fields[_clstable(key)] = field
         cls.fields = MappingProxyType(fields)
+
+def _switch_field_types(cls, tabname):
+    tables = (table for table in Table.subtables() if table.__name__ != tabname)
+    for table in tables:
+        for field in table.fields.values():
+            try: type = field.type.__args__[0]
+            except: type = field.type
+
+            if type == tabname: 
+                field.__dict__['type'] = cls
 
 def _clstable(clstable: str) -> str:
     if table := re.findall(r'[A-Z][a-z]+', clstable):
@@ -56,12 +70,12 @@ def _field_handle(clsname, field: Field, type_: type | str) -> Field:
         field.__dict__['type'] = type_in_iter if type_in_iter else type_
         linked_field = _linked_field(clsname, field)
         attitudes = _attitudes_tab_field(field, linked_field) if is_table \
-               else _attitudes_seq_field(linked_field)
+               else _attitudes_iter_field(linked_field)
 
         if len(attitudes) > 1:
             data_field['attitude'], linked_field.__dict__['attitude'] = attitudes
         else:
-            data_field['attitude'] = attitudes
+            data_field['attitude'] = attitudes[0]
     
     return Field(**data_field)
 
@@ -71,11 +85,11 @@ def _linked_field(clsname, field: Field) -> Field | None:
     for linked_field in field.type.fields.values():
         try: type_ = linked_field.type.__args__[0]
         except: type_ = linked_field.type
-        if type_.__name__ == clsname:
+        if _is_table(type_) and type_.__name__ == clsname:
             return linked_field
 
 def _is_table(type_) -> bool:
-    return type(type_) == str or 'Table' in (sup.__name__ for sup in type_.__bases__)
+    return type(type_) == str or issubclass(type_, Table)
 
 def _type_iter_table(type_):
     is_sequence = type_ is not str and issubclass(type(type_()), Iterable)
@@ -93,13 +107,28 @@ def _attitudes_tab_field(field, linked_field) -> tuple[TA] | tuple[TA, TA]:
     else:
         return (TA.OneToOne,) if field.unique else (TA.ManyToOne,)
 
-def _attitudes_seq_field(linked_field) -> tuple[TA] | tuple[TA, TA]:
+def _attitudes_iter_field(linked_field) -> tuple[TA] | tuple[TA, TA]:
     if linked_field and linked_field.attitude is TA.ManyToOne:
         return (TA.OneToMany,)
     elif linked_field and linked_field.attitude is TA.OneToMany:
         return (TA.ManyToMany,)*2
     else:
         return (TA.OneToMany,)
+
+
+class Table(metaclass=MetaTable):
+    fields: MappingProxyType[str, Field]
+    table: str
+
+    @classmethod
+    def subtables(cls) -> list['Table']:
+        subclasses = []
+        for sub in cls.__subclasses__():
+            if sub.__name__ == 'Table':
+                subclasses += sub.subtables()
+            else:
+                subclasses.append(sub)
+        return subclasses
 
 
 async def init():
--- database/table.py
+++ database/table.py
@@ -1,40 +1,89 @@
 import asyncio, re
+from collections import defaultdict, namedtuple
+from typing import Iterable
 
-from meta import init, MetaTable, Field
+from functions import Row, fetch
+from meta import init, Table, Field, TablesAttitude as TA
 
 
-Table = type
+class Table(Table):
+    @classmethod
+    async def select(cls) -> list[Row] | dict[Row, Row]:
+        data = args, keys, params, joins = tuple(map(set, ((),)*4))
+        for name, field in cls.fields.items():
+            cls._field_handle(name, field, *data)
 
-class MetaTable(MetaTable):
-    def __init__(cls, clsname, superclasses, attributedict):
-        if clsname == 'Table': return
-        _switch_field_types(cls, clsname)
-        super().__init__(clsname, superclasses, attributedict)
+        params = ', '.join(params)
+        joins = '\n'.join(joins)
+        query = f'SELECT {params} FROM {cls.table} '+joins
 
-def _switch_field_types(cls, tabname):
-    tables = (table for table in Table.__subclasses__() if table.__name__ != tabname)
-    for table in tables:
-        for field in table.fields.values():
-            try: type = field.type.__args__[0]
-            except: type = field.type
+        output = await fetch(query) 
+        return cls._keys_handle(args, keys, output)
+    
+    @classmethod
+    def _field_handle(cls, name, field, args, keys, params, joins):
+        if field.attitude is TA.Simple:
+            args.add(name)
+            params.add(name)
+        else:
+            table = field.type
+            fields = tuple(n for n, f in table.fields.items()
+                           if f.attitude is TA.Simple)
 
-            if type == tabname: 
-                field.__dict__['type'] = cls
+            args.update(table.table+'_'+f for f in fields)
+            params.update('{0}.{1} AS {0}_{1}'.format(table.table, n) 
+                          for n in fields)
+            if key := cls._key(table, field, fields):
+                keys.update(key)
+            join_form = '{} '+f' {table.table} '+'USING({})'
+            joins.add(join_form.format(*cls._join(table, field)))
 
-class Table(metaclass=MetaTable):
-    fields: dict[str, Field]
-    table: str
+    @classmethod
+    def _key(cls, table: Table, field: Field, fields) -> Iterable[str] | None:
+        if field.attitude is TA.OneToMany:
+            return (n for n, f in cls.fields.items() if f.attitude is TA.Simple)
+        elif field.attitude is TA.ManyToOne:
+            return (table.table+'_'+f for f in fields)
+
+    @classmethod
+    def _join(cls, table: Table, field: Field) -> tuple[str, str]:
+        if field.attitude is TA.OneToOne:
+            return 'LEFT JOIN', f'{table.table}_id'
+        elif field.attitude is TA.OneToMany:
+            return 'LEFT JOIN', f'{cls.table}_id'
+        else:
+            return 'RIGHT JOIN', f'{table.table}_id'
+
+
+    @classmethod
+    def _keys_handle(cls, args, keys, output):
+        if keys := args & keys:
+            output_ = defaultdict(list)
+            KeyRecord = namedtuple('Record', keys)
+            RowRecord = namedtuple('Record', args-keys)
+            for row in output:
+                row = dict(row)
+                key = KeyRecord(*(row.pop(key) for key in keys))
+                row = RowRecord(*row.values())
+                output_[key].append(row)
+            output = dict(output_)
+        return output
+
 
 class Country(Table):
     name: str
-    inventory: list['Inventory']
+
+class Item(Table):
+    name: str
 
 class Inventory(Table):
     count: int
-    country: Country
+    item: Item = Field(unique=True)
+    country: Country = Field(unique=True)
 
 async def main():
     await init()
+    print(await Inventory.select())
 
 if __name__ == '__main__':
     asyncio.run(main())
